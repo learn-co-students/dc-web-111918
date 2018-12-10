61ec0b7230e60c8c7f9d42e4f71802b343a4f375
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2018-12-10 10:08:23 -0500

    prelecture 12

--- 11-intro-to-testing/palindrome.rb
+++ 11-intro-to-testing/palindrome.rb
@@ -2,13 +2,18 @@ require 'pry'
 
 class PalindromeChecker
 
-    # def is_palindrome?(str)
-    #     if str.length <= 1
-    #         return true
-    #     elsif str[0] != str[-1]
-    #         return false
-    #     else
-    #         return is_palindrome?(str[1..-2])
-    #     end
-    # end
+    def is_palindrome?(word)
+        raise ArgumentError if !(word.is_a? String)
+        word.downcase!
+        word.gsub!(" ", "")
+        word.gsub!(/[^a-z0-9\s]/, "") #regex to match anything not a letter, number or whitespace
+        if word.length <= 1
+            return true
+        elsif word[0] == word[-1]
+            return is_palindrome?(word.slice(1..-2))
+        else
+            return false
+        end
+    end
+
 end
