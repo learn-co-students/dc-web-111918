756b0996e88e1244a39a6a33f8c77e824ec29132
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2018-12-19 10:05:24 -0500

    prelecture 19

---
+++ 19-rails-validations/snack-tracker/app/controllers/snacks_controller.rb
@@ -0,0 +1,45 @@
+class SnacksController < ApplicationController
+
+  before_action :find_snack, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @snacks = Snack.all
+  end
+
+  def show
+  end
+
+  def new
+    @snack = Snack.new
+  end
+
+  def create
+    @snack = Snack.create(strong_params)
+    redirect_to @snack
+  end
+
+  def edit
+  end
+
+  def update
+    @snack.update(strong_params)
+    redirect_to @snack
+
+  end
+
+  def destroy
+    @snack.destroy
+    redirect_to snacks_path
+  end
+
+  private
+
+  def strong_params
+    params.require(:snack).permit(:name, :tastiness, :calories, :retailer_id)
+  end
+
+  def find_snack
+    @snack = Snack.find(params[:id])
+  end
+
+end
